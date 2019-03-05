c0f128ed2782bd61341e8c7f27aa8299ed17fc1e
Calasada <carlyodes1@gmail.com>
2019-03-05 15:59:27 -0500

    Add Controller Support

--- sketch.js
+++ sketch.js
@@ -1,10 +1,38 @@
+let connected = false;
+let hasGP = false;
+let gp;
+
+let fb = 0;
+let rl = 0;
+let rot = 0;
+
 function setup() {
   createCanvas(700, 700);
   background(70);
 }
 
+$(window).on("gamepadconnected", function() {
+  connected = true;
+  $("#gamepadPrompt").html("Gamepad connected!");
+  console.log("connection event");
+});
+
+var checkGP = window.setInterval(function() {
+  console.log('checkGP');
+  if(navigator.getGamepads()[0]) {
+      if(!hasGP) $(window).trigger("gamepadconnected");
+      window.clearInterval(checkGP);
+  }
+}, 500);
+
 function draw() {
   stroke(255);
   noFill();
   square(width/2, height/2, 40);
+  if(connected) {
+    gp = navigator.getGamepads()[0];
+    fb = -gp.axes[1];
+    rl = gp.axes[0];
+    rot = gp.axes[2];
+  }
 }
