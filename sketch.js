108b6d41af05326f8fc518215a49cc846f2ca660
Calasada <carlyodes1@gmail.com>
2019-03-05 16:18:59 -0500

    Add Control of Square

--- sketch.js
+++ sketch.js
@@ -6,9 +6,18 @@ let fb = 0;
 let rl = 0;
 let rot = 0;
 
+let x;
+let y;
+let angle;
+
+const speed = 4;
+
 function setup() {
   createCanvas(700, 700);
   background(70);
+  x = width/2;
+  y = height/2;
+  angle = 0;
 }
 
 $(window).on("gamepadconnected", function() {
@@ -26,12 +35,25 @@ var checkGP = window.setInterval(function() {
 }, 500);
 
 function draw() {
+  background(70);
+
+  if(Math.abs(rl) > 0.2 || Math.abs(fb) > 0.2) {
+    x += rl*speed;
+    y += fb*speed;
+  }
+  if(Math.abs(rot) > 0.2) {
+    angle += rot*0.05;
+  }
+  translate(x, y);
+  rotate(angle);
+
   stroke(255);
   noFill();
-  square(width/2, height/2, 40);
+  square(-20, -20, 40);
+
   if(connected) {
     gp = navigator.getGamepads()[0];
-    fb = -gp.axes[1];
+    fb = gp.axes[1];
     rl = gp.axes[0];
     rot = gp.axes[2];
   }
